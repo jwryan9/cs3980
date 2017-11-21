5c5fb94020a70816213870193d4438d67b4a616e
Jason Ryan <jason.w.ryan9@gmail.com>
2017-11-21 01:57:34 -0600

    Completed JSON implementation.

--- ProgPractice/ProgPractice10/speakers.js
+++ ProgPractice/ProgPractice10/speakers.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-	
+    var mainElement = document.getElementsByTagName("main")[0];
+    document.addEventListener('click', function (event) {
+        var clickedTitle = event.target.title;
+
+        if(clickedTitle) {
+            var jsonLink = "json_files/" + clickedTitle + ".json";
+
+            $.getJSON(jsonLink, function (data) {
+                $.each(data, function () {
+                    $.each(this, function (key, value) {
+                        mainElement.innerHTML = "";
+                        mainElement.innerHTML += "<h1>" + value.title + "</h1>";
+                        mainElement.innerHTML += "<img src=" + value.image + ">";
+                        mainElement.innerHTML += "<h2>" + value.month + "<br>" + value.speaker + "</h2>";
+                        mainElement.innerHTML += "<p>" + value.text + "</p>";
+                    });
+                });
+
+
+            });
+        }
+    });
 }); // end ready
